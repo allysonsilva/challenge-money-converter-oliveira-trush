0d37b7df514a16d08ad080d7361d05c7617a06be
Allyson Silva <19507773+allysonsilva@users.noreply.github.com>
2022-07-27 19:25:19 -0300

    refactor: Removed default routes from `routes` folder 🧽

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +11,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('info', function () {
+// });
